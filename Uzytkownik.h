ac7c44d09dfa98ec31ecebc30c09a8048d451659
Mateusz Szwed <mateusz.szwed.programista@gmail.com>
2022-06-08 14:08:58 +0200

    Divide KsiazkaAdresowa to smaller class

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -1,4 +1,8 @@
+#ifndef UZYTKOWNIK_H
+#define UZYTKOWNIK_H
+
 #include <iostream>
+
 
 using namespace std;
 
@@ -17,3 +21,5 @@ public:
  string pobierzLogin();
  string pobierzHaslo();
 };
+
+#endif // UZYTKOWNIK_H
