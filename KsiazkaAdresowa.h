849a27b5d73037d09b096057882902985a456c31
Mateusz Szwed <mateusz.szwed.programista@gmail.com>
2022-06-09 13:23:03 +0200

    Added filename at begining

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -13,7 +13,9 @@ UzytkownikMenedzer uzytkownikMenedzer;
 
 public:
 
-    KsiazkaAdresowa();
+    KsiazkaAdresowa(string nazwaPlikuZUzytkownikami) : uzytkownikMenedzer(nazwaPlikuZUzytkownikami) {
+    uzytkownikMenedzer.wczytajUzytkownikowZPliku();
+    };
 
 void rejestracjaUzytkownika();
 void wyswietlWszyskichUzytkownikow();
